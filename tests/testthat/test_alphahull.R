7f3cccce71eed260f912dcd413d9bea91da220bb
harriet-mason <harriet.mason@monash.edu>
2021-09-16 16:57:31 +1000

    skinny and convex tests

---
+++ tests/testthat/test_alphahull.R
@@ -0,0 +1,23 @@
+# Load Libraries
+library(dplyr)
+
+# Set Seed
+set.seed(4213)
+
+# Generate circle of data for test
+data <- tibble(r = runif(20000, 0,1),
+               theta = runif(20000, 0, 2*pi)) %>%
+  mutate(x = r*cos(theta),
+         y = r*sin(theta))
+
+plot(data$x,data$y)
+
+# Test Skinny Scagnostic
+test_that("Stringy Scagnotist", {
+  expect_lt(sc_skinny(data$x,data$y), 0.1)
+})
+
+# Test Convex Scagnostic
+test_that("Convex Scagnotist", {
+  expect_gt(sc_convex(data$x,data$y), 0.95)
+})
