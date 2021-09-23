c9ac254132560e4cc9326617018ea472e1dbb3b6
harriet-mason <harriet.mason@monash.edu>
2021-09-23 14:57:44 +1000

    add in sin wave test for dcor and splines

--- tests/testthat/test_association.R
+++ tests/testthat/test_association.R
@@ -1,3 +1,11 @@
 library(dplyr)
 library(cassowaryr)
 library(usethis)
+
+#generate sin wave test
+x <- seq(-pi, pi, 0.01)
+y <- sin(x)
+
+test_that("dcor vs splines", {
+  expect_false(isTRUE(all.equal(sc_splines(x,y), sc_dcor(x,y))))
+})
