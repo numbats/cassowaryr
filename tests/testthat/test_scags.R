2b260d1c7fa6ac7846e8b964fea3ec844d3fa3fb
harriet-mason <harriet.mason@monash.edu>
2021-09-25 20:37:13 +1000

    inc sample size to 1 to show striated_adjusted spanning almost perfectly from 0 to 1 for specific plots

--- tests/testthat/test_scags.R
+++ tests/testthat/test_scags.R
@@ -16,10 +16,10 @@ ggpairs(s, columns = 2:8)
 # Compare striated to striated adjusted
 
 # Simulated data for what it is looking for
-d1 <- sample(1:10, 100, replace=TRUE)
-d2 <- sample(1:10, 100, replace=TRUE)
-c1 <- rnorm(100)
-c2 <- rnorm(100)
+d1 <- sample(1:10, 1000, replace=TRUE)
+d2 <- sample(1:10, 1000, replace=TRUE)
+c1 <- rnorm(1000)
+c2 <- rnorm(1000)
 #rotated
 x3 <- c(c1, c2)
 y3 <- c(1.5*c1+1, 1.5*c2+3)
@@ -27,16 +27,20 @@ y3 <- c(1.5*c1+1, 1.5*c2+3)
 #most striated_adjusted (both discrete)
 sc_striated_adjusted(d1,d2)
 sc_striated(d1,d2)
+plot(d1,d2)
 
 #next most (one variable is discrete)
 sc_striated_adjusted(d1,c1)
 sc_striated(d1,c1)
+plot(d1,c1)
 
 # least (both continuous)
 sc_striated_adjusted(c1,c2)
 sc_striated(c1,c2)
+plot(c1,c2)
 
 # rotated (both continuous)
 sc_striated_adjusted(x3,y3)
 sc_striated(x3,y3)
+plot(x3,y3)
 
