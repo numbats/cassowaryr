8af42c2e4a9a86665d1c92f6be22d8322218ac30
harriet-mason <harriet.mason@monash.edu>
2021-09-28 23:29:13 +1000

    working verion of clumpy_robust (sort of)

--- R/robustscags.R
+++ R/robustscags.R
@@ -1,17 +1,47 @@
+#' Compute robust clumpy scagnostic measure using MST
+#'
+#' @examples
+#'   require(ggplot2)
+#'   require(tidyr)
+#'   require(dplyr)
+#'   ggplot(datasaurus_dozen, aes(x=x, y=y)) +
+#'     geom_point() +
+#'     facet_wrap(~dataset, ncol=3, scales = "free")
+#'   sc_clumpy_robust(datasaurus_dozen_wide$away_x, datasaurus_dozen_wide$away_y)
+#'   sc_clumpy_robust(datasaurus_dozen_wide$dots_x, datasaurus_dozen_wide$dots_y)
+#' @export
+sc_clumpy_robust <- function(x, y) UseMethod("sc_clumpy_robust")
 
-#function
-clumpy_robust <- function(){
-
+#' @rdname clumpy_robust
+#' @export
+sc_clumpy_robust.scree <- function(x, y = NULL) {
+  #generate vector of MST edges
+  mymst <- gen_mst(x$del, x$weights)
+  sc_clumpy_robust.igraph(mymst, x)
 }
 
-#function used inside robust clumpy
-inner_clumpy <- function(mymst, x){
-  #pretty similar to original clumpy with with enough changes that I'm just making a new function
-  #input: mst and scree
-  #output: Clumpy value and the respective EK and EJ edges
+#' @rdname clumpy_robust
+#' @export
+sc_clumpy_robust.default <- function(x, y){
+  sc <- scree(x, y)
+  sc_clumpy_robust.scree(sc)
+}
 
-  #lower triangular matrix
-  mstmat <- twomstmat(mymst,x)$lowertri
+#' @rdname clumpy_robust
+#' @export
+sc_clumpy_robust.igraph <- function(mst, scr){
+  mst_lt <- twomstmat(mst,scr)$lowertri
+  vals <- outside_cluster(mst_lt)
+  n <- length(which(mst_lt>0))
+  sum(vals)/n
+}
+
+#function used inside robust clumpy_robust
+
+inner_clumpy <- function(mstmat){
+  #pretty similar to original clumpy with with enough changes that I'm just making a new function
+  #input: mst (lower triangular matrix) and scree
+  #output: Clumpy value and the respective EK and EJ edges
 
   #make index variables to iterate through
   matind <- which(mstmat>0)
@@ -84,42 +114,100 @@ inner_clumpy <- function(mymst, x){
     denom <- (length(c1ind)+length(c2ind))*ej_weight
 
     #maintain vectors
-    clumpy[j] <- 1 - (numer1+numer2)/denom #ifelse(denom==0, 0, 1 - (numer1+numer2)/denom) #set =0 if denom=0
+    clumpy[j] <- ifelse(denom==0, 0, 1 - (numer1+numer2)/denom) #set =0 if denom=0
     ej[j] <- ej_weight
 
   }
   # return final clumpy measure
   ind <- which(clumpy==max(clumpy, na.rm=TRUE)) #which.max(clumpy)
-  c(clumpy[ind], ej[ind]) #return c(clumpy val, ej weight)
-
+  c(clumpy[ind], ej[ind], matind[ind]) #return c(clumpy val, ej weight, ind in matrix)
 }
 
 #check is need to go deeper on hierarchy bit
-split_check <- function(mst, scr){
+outside_cluster <- function(mstmat_lt){
 
-  #get w value (basically straight from outlying_identify, make own function maybe)
-  matlist <- twomstmat(mst,scr)
-  mstmat_lt <- matlist$lowertri
-  edges <- mstmat_lt[which(mstmat_lt>0)]
-  w <- psi(edges)
+  # find outlier threshold
+  edges <- mstmat_lt[which(mstmat_lt>0)] #get edges
+  w <- psi(edges) #set threshold for outliers
+
+  # TODO: REMOVE OUTLIERS
+
 
   #get clumpy value
-  clump_ej <- inner_clumpy(mst, scr)
+  clump_ej <- inner_clumpy(mstmat_lt)
 
   #no split
   if(clump_ej[2]<w){
-    val <- clump_ej[1]
+    val <- length(edges)*clump_ej[1]
   }
   #split
   if(clump_ej[2]>=w){
-    print("big")
-    print(clump_ej[2])
-    print(w)
+    #split into two MST
+    newclusters <- split_clusters(mstmat_lt, clump_ej[3])
+    val <- c(outside_cluster(newclusters$mat1), outside_cluster(newclusters$mat2))
   }
-
+  val #retun value
 }
 
-split_clusters <- function(mst, scr){
+split_clusters <- function(mst_lt, ind, splitforclumpy=FALSE){
+  #make index variables to iterate through
+  matind <- which(mst_lt>0)
+  j <- which(matind == ind)
 
+  #have two clusters sprawling out from the deleted edge (inex i)
+  c1rowcol <- (matind %% length(mst_lt[,1]))[j]
+  c2rowcol <- ((matind-1) %/% length(mst_lt[,1]) +1)[j]
+
+  #remove ej
+  if (splitforclumpy==FALSE){
+    mst_lt[ind] = 0
+  }
+    #and all values in mst that are greater than ej if inside clumpy calc
+  if (splitforclumpy==TRUE){
+    ej_weight <- mst_lt[ind]
+    mst_lt[which(mst_lt >= ej_weight)] = 0
+  }
+
+  #remake index objects to edit within iteration
+  matind_ej <- which(mst_lt>0)
+  rows_ej <- matind_ej %% length(mst_lt[,1])
+  cols_ej <- (matind_ej-1) %/% length(mst_lt[,1]) +1
+
+  #initialise variable that checks if clusters have changed
+  whilecheck <- c(c1rowcol,c2rowcol)
+  quitloop = 0
+
+  while(quitloop==0){
+
+    # find matches in rows/columns to join connected vertices
+    c1rowcol <- unique(c(c1rowcol,
+                         rows_ej[which(cols_ej%in%c1rowcol)],
+                         cols_ej[which(rows_ej%in%c1rowcol)]))
+    c2rowcol <- unique(c(c2rowcol,
+                         rows_ej[which(cols_ej%in%c2rowcol)],
+                         cols_ej[which(rows_ej%in%c2rowcol)]))
+
+    # check if indices are done updating
+    if(setequal(whilecheck, c(c1rowcol,c2rowcol))){
+      quitloop=1
+    }
+
+    #update while loop check
+    whilecheck = c(c1rowcol,c2rowcol)
+
+  }
+
+  #get matrix indices of each of the clusters
+  c1ind <- unique(c(matind_ej[which(cols_ej%in%c1rowcol)],
+                    matind_ej[which(rows_ej%in%c1rowcol)]))
+  c2ind <- unique(c(matind_ej[which(cols_ej%in%c2rowcol)],
+                    matind_ej[which(rows_ej%in%c2rowcol)]))
+  #make two different adjacency matrices based on two clusters
+  mst1 <- mst_lt
+  mst1[c2ind] = 0
+  mst2 <- mst_lt
+  mst2[c1ind] = 0
+  list(mat1 = mst1,
+       mat2 = mst2)
 }
 
