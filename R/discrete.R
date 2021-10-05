8dd4c543df4c7f747c86b142d9278e53b3e68252
dicook <visnut@gmail.com>
2021-10-05 16:32:59 +1100

    oooh, have the stripes index working now

--- R/discrete.R
+++ R/discrete.R
@@ -1,10 +1,10 @@
 #' Measure of Discreteness
 #'
-#' This metric computes the ration between the
-#' number of unique values to total data values
+#' This metric computes the 1-(ratio between the
+#' number of unique values to total data values)
 #' on number of rotations of the data, and
 #' returns the smallest value. If this value is
-#' small it means that there are only a few unique
+#' large it means that there are only a few unique
 #' data values, and hence the distribution is
 #' discrete
 #'
@@ -19,21 +19,25 @@
 #'            datasaurus_dozen_wide$dino_y)
 #' @export
 sc_striped <- function(x, y){
-  # TODO: Need to make this work for rotations,
-  # and do some rounding, bin first and then
-  # the rounding isn't necessary
-  dx <- unique(x)
-  value <- 1-length(dx)/length(x)
-  xs <- x - mean(x)
-  ys <- y - mean(y)
+  # Put both variables in range of -1 to 1
+  xs <- 2 * ((x - min(x, na.rm = T))/
+    (max(x, na.rm = T) - min(x, na.rm = T)) - 0.5)
+  ys <- 2 * ((y - min(y, na.rm = T))/
+               (max(y, na.rm = T) - min(y, na.rm = T)) - 0.5)
+  # Bin to effectively round real values
+  bx <- cut(xs, 50)
+  # Only bins with observations
+  dx <- unique(bx)
+  # This is the index value
+  value <- 1-length(dx)/length(levels(bx))
+  # Check for a full range of 2D rotations
   for (ang in seq(0, pi, pi/180)) {
     xr <- sin(ang)*xs + cos(ang)*ys
-    #yr <- -cos(ang)*xs + sin(ang)*ys
-    dx <- unique(xr)
-    new_value <- 1-length(dx)/length(x)
+    bx <- cut(xr, 50)
+    dx <- unique(bx)
+    new_value <- 1-length(dx)/length(levels(bx))
     if (new_value > value) {
       value <- new_value
-      cat(value, " ", new_value, "\n")
     }
   }
   value
