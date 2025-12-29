438c4968b149592b8aee2dbe080551dbdd83866f
harriet-mason <harriet.mason@monash.edu>
2025-12-29 20:36:46 +1100

    added test cases for outlier removal (basically test cases for outlying)

---
+++ tests/testthat/test-scree.R
@@ -0,0 +1,84 @@
+library(ggplot2)
+
+# No Outliers
+x1 <- c(0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1)
+y1 <- c(0,0,1,2,2,3,4,4,5,6,6,7,8,8,9,10)
+ggplot() + geom_point(aes(x1,y1))
+
+draw_mst(x1,y1, outlier_rm = FALSE) +
+  xlim(0,1) + ylim(0,1)
+
+draw_mst_old(x1,y1, out.rm=TRUE)+
+  xlim(0,1) + ylim(0,1)
+
+# new version
+draw_mst(x1,y1, outlier_rm = TRUE)+
+  xlim(0,1) + ylim(0,1)
+
+# One outlier
+x2 <- c(x1, 10)
+y2 <- c(y1,10)
+ggplot() + geom_point(aes(x2,y2))
+draw_mst(x2,y2, outlier_rm = FALSE) +
+  xlim(0,1) + ylim(0,1)
+draw_mst_old(x2,y2, out.rm=TRUE)+
+  xlim(0,1) + ylim(0,1)
+draw_mst(x2,y2, outlier_rm = TRUE)+
+  xlim(0,1) + ylim(0,1)
+
+
+# Outlier with a Close Value (so no outliers)
+x3 <- c(x2, 10)
+y3 <- c(y2, 9)
+# plot of points
+ggplot() + geom_point(aes(x3,y3))
+
+# no outlier removal
+draw_mst(x3,y3, outlier_rm = FALSE) +
+  xlim(0,1) + ylim(0,1)
+
+# outlier removal, old version
+draw_mst_old(x3,y3, out.rm=TRUE)+
+  xlim(0,1) + ylim(0,1)
+
+# new version
+draw_mst(x3,y3, outlier_rm = TRUE)+
+  xlim(0,1) + ylim(0,1)
+
+# Two connected outliers
+x4 <- c(x2, 10)
+y4 <- c(y2, 7)
+# plot of points
+ggplot() + geom_point(aes(x4,y4))
+
+# no outlier removal
+draw_mst(x4,y4, outlier_rm = FALSE) +
+  xlim(0,1) + ylim(0,1)
+
+# outlier removal, old version
+draw_mst_old(x4,y4, out.rm=TRUE)+
+  xlim(0,1) + ylim(0,1)
+
+# new version
+draw_mst(x4,y4, outlier_rm = TRUE)+
+  xlim(0,1) + ylim(0,1)
+
+
+# Internal outlier
+x6 <- c(0,1,1,1,0,0,0,0,0,0,0,1,1,1, 10, 10, 10)
+y6 <- c(0,0,1,2,2,3,4,5,6,7,8,8,9,10, 10, 6, 7)
+# plot of points
+ggplot() + geom_point(aes(x6,y6))
+
+# no outlier removal
+draw_mst(x6,y6, outlier_rm = FALSE) +
+  xlim(0,1) + ylim(0,1)
+
+# outlier removal, old version
+draw_mst_old(x6,y6, out.rm=TRUE)+
+  xlim(0,1) + ylim(0,1)
+
+# new version
+draw_mst(x6,y6, outlier_rm = TRUE)+
+  xlim(0,1) + ylim(0,1)
+
