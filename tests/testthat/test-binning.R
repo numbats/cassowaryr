0693a74cfc0cd95a62311a376606a5321c94cf79
harriet-mason <harriet.mason@monash.edu>
2025-12-31 20:34:47 +1100

    moved commented code

--- tests/testthat/test-binning.R
+++ tests/testthat/test-binning.R
@@ -4,12 +4,14 @@ y <- rnorm(1000)
 
 # get matrix object
 xy <- cbind(unitize(x), unitize(y))
-hexbin_xy <- readRDS("tests/testthat/objects/hexbin_xy.rds")
 
+# get binned object
 # code that made hexbin_xy
 # hexbin_xy <- get_binned_matrix(xy, "hex")
 # saveRDS(hexbin_xy,
 #         file = "tests/testthat/objects/hexbin_xy.rds")
+hexbin_xy <- readRDS("tests/testthat/objects/hexbin_xy.rds")
+
 
 test_that("Test get_binned_matrix", {
   # no binning
